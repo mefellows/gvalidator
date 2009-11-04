14468335fa5db5b99d2e4ed1411f30447b1bac4d
matt.fellows@onegeek.com.au <matt.fellows@onegeek.com.au@12492026-9054-0410-bb6e-7f8f93f34f08>
2009-11-04 21:08:19 +0000

    Initial plugin file example

---
+++ js/gvalidator-plugins.js
@@ -0,0 +1,24 @@
+ONEGEEK.forms.GValidator.options = {
+    // Constants: Edit to suit your environment
+    autoformValidation: true,
+    compactMessages: true,
+
+    // Image constants
+    icons: {
+      error:  '../images/icons/cross.png',
+      ok:     '../images/icons/tick.png',
+      info:   '../images/icons/help.png',
+      alert:  '../images/icons/icon_alert.gif'
+    }
+};
+
+ONEGEEK.forms.GValidator.plugins = {
+    ip4address: {
+        _extends:     'GenericTextField',
+        regex:        /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/g,
+        cleanRegex:   /[^0-9\.]/g,
+        contextMessage: 'Please enter a valid IP 4 Address i.e. 127.0.0.1',
+        errorMessage: 'Please enter a valid IP 4 Address i.e. 127.0.0.1',   
+        successMessage: 'Thanks'  
+    }    
+};
