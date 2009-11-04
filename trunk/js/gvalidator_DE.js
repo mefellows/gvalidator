f0b9bfb8e60880966eff7598eec54f7a5bce56d1
matt.fellows@onegeek.com.au <matt.fellows@onegeek.com.au@12492026-9054-0410-bb6e-7f8f93f34f08>
2009-11-04 21:07:47 +0000

    Added German translation file for test_DE.html

---
+++ trunk/js/gvalidator_DE.js
@@ -0,0 +1,15 @@
+ONEGEEK.forms.GValidator.translation.DE = {  
+    defaults: {
+      successMessage: 'Danke',
+      contextMessage: 'Bitte füllen Sie',
+      errorMessage: 'Kaputt! Es wurde ein Fehler beim Überprüfen diesem Bereich'
+    },        
+    ip4address :{
+      successMessage :'Danke schoen!',
+      contextMessage :'Bitte geben Sie eine gültige IP-Adresse 4 i.e. 127.0.0.1',
+      errorMessage :'Bitte geben Sie eine gültige IP-Adresse 4 i.e. 127.0.0.1',
+    },
+    firstname: {
+      contextMessage: 'Wir möchten Sie von Ihrem Namen zu nennen',
+    }  
+};
