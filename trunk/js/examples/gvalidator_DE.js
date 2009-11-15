fec78cbb484508021c6af31685ae38dff28dc5ea
matt.fellows@onegeek.com.au <matt.fellows@onegeek.com.au@12492026-9054-0410-bb6e-7f8f93f34f08>
2009-11-15 13:04:10 +0000

    Added alert message translation

--- trunk/js/examples/gvalidator_DE.js
+++ trunk/js/examples/gvalidator_DE.js
@@ -7,9 +7,13 @@ ONEGEEK.forms.GValidator.translation.DE = {
     ip4address :{
       successMsg :'Danke schoen!',
       contextMsg :'Bitte geben Sie eine gültige IP-Adresse 4 i.e. 127.0.0.1',
-      errorMsg :'Bitte geben Sie eine gültige IP-Adresse 4 i.e. 127.0.0.1',
+      errorMsg :'Bitte geben Sie eine gültige IP-Adresse 4 i.e. 127.0.0.1'
     },
     firstname: {
-      contextMsg: 'Wir möchten Sie von Ihrem Namen zu nennen',
+      contextMsg: 'Wir möchten Sie von Ihrem Namen zu nennen'
     }  
+};
+
+ONEGEEK.forms.GValidator.options = {  
+    fMsg: "Bitte korrigieren Sie die markierten Fehler!"
 };
